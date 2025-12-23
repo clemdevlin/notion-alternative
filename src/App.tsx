aad8cfe5fa1a71a80105dc0300d9d3b6b3e1cbed
Clement <egyayemi2006@gmail.com>
2025-12-23 02:25:29 +0000

    Simplify App layout by removing sidebar logic
    Removed sidebar collapse logic and simplified layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { AppProvider, useApp } from './context/AppContext';
+import { AppProvider } from './context/AppContext';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 
 const App: React.FC = () => {
-  const {isSidebarCollapsed} = useApp()
   return (
     <AppProvider>
       <div className="flex h-screen bg-gray-100">
         <Sidebar />
-        <div className={`${isSidebarCollapsed ? 'ml-16' : 'ml-64'}`} >
-          <MainContent />
-        </div>
+        <MainContent />
       </div>
     </AppProvider>
   );
