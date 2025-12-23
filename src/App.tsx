ed9d3e68d17c880d62579adb5458df3cb2db0c84
Clement <egyayemi2006@gmail.com>
2025-12-23 02:20:45 +0000

    Integrate useApp hook for sidebar collapse state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { AppProvider } from './context/AppContext';
+import { AppProvider, useApp } from './context/AppContext';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 
 const App: React.FC = () => {
+  const {isSidebarCollapsed} = useApp()
   return (
     <AppProvider>
       <div className="flex h-screen bg-gray-100">
         <Sidebar />
-        <div>
+        <div className={`${isSidebarCollapsed ? 'ml-16' : 'ml-64'}`} >
           <MainContent />
         </div>
       </div>
